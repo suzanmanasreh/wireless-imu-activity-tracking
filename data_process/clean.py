c73f9b251b4f8665d6b8214d5770ef62c3c0dd9b
smanasreh6 <smanasreh6@gatech.edu>
2025-10-07 15:37:49 -0400

    fill in NaNs at top

--- data_process/clean.py
+++ data_process/clean.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd
 import sys
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Conv1D, MaxPooling1D, Flatten
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import classification_report, accuracy_score
 import matplotlib.pyplot as plt
 
 
@@ -15,8 +20,6 @@ def add_data(hmap, data, ts):
 training_files = ["../data/bicep_curl/suzan_bicep_set1.log", "../data/bicep_curl/jake_bicep_set1.log", "../data/bicep_curl/udai_bicep_set1.log", "../data/shoulder_press/suzan_shoulder_set1.log", "../data/shoulder_press/jake_shoulder_set1.log", "../data/shoulder_press/udai_shoulder_set1.log", "../data/row/suzan_row_set1.log", "../data/row/jake_row_set1.log", "../data/row/udai_row_set1.log", "../data/rdl/suzan_rdl_set1.log", "../data/rdl/jake_rdl_set1.log", "../data/rdl/jessica_rdl_set1.log", "../data/squat/suzan_squat_set1.log", "../data/squat/jake_squat_set1.log", "../data/squat/udai_squat_set1.log"]
 test_files = ["../data/bicep_curl/suzan_bicep_set2.log", "../data/bicep_curl/jake_bicep_set2.log", "../data/bicep_curl/udai_bicep_set2.log", "../data/shoulder_press/suzan_shoulder_set2.log", "../data/shoulder_press/jake_shoulder_set2.log", "../data/shoulder_press/udai_shoulder_set2.log", "../data/row/suzan_row_set2.log", "../data/row/jake_row_set2.log", "../data/row/udai_row_set2.log", "../data/rdl/suzan_rdl_set2.log", "../data/rdl/jake_rdl_set2.log", "../data/rdl/jessica_rdl_set2.log", "../data/squat/suzan_squat_set2.log", "../data/squat/jake_squat_set2.log", "../data/squat/udai_squat_set2.log"]
 labels = ["bicep_curl", "bicep_curl", "bicep_curl", "shoulder_press", "shoulder_press", "shoulder_press", "row", "row", "row", "rdl", "rdl", "rdl", "squat", "squat", "squat"]
-y_train = np.asarray(labels, dtype="object")
-y_train = pd.DataFrame(y_train)
 X_train = []
 X_test = []
 
@@ -94,12 +97,14 @@ def create_df(filename, dfs):
 
         df = pd.DataFrame({'timestamp': timestamps, 'accel_x': accel_x.values(), 'accel_y': accel_y.values(), 'accel_z': accel_z.values(), 'gyro_x': gyro_x.values(), 'gyro_y': gyro_y.values(), 'gyro_z': gyro_z.values()})
         # df.to_csv('raw_df.csv')
-        cleaned_df = df.interpolate(method="linear")
+        # df = df.set_index("timestamp")
+        cleaned_df = df.interpolate(method="linear", limit_direction="both")
         name = "../csvs/" + filename.split("/")[-1].split(".")[0] + "_df.csv"
         if len(sys.argv) > 1 and sys.argv[1] == "write":
             cleaned_df.to_csv(name, index=False)
         print(cleaned_df.head())
         dfs.append(cleaned_df)
+        return cleaned_df
         # cleaned_df.plot(x="timestamp")
         # plt.title("Bicep Curls")
         # plt.xlabel("Timestamp")
@@ -107,11 +112,28 @@ def create_df(filename, dfs):
         # plt.show()
 
 
+def segmentation(df, window_size = 5, step_size = 2, label=None):
+    segments = []
+    labels = []
+    for start in range(0, len(df) - window_size, step_size):
+        end = start + window_size
+        window = df.iloc[start:end][['accel_x', 'accel_y', 'accel_z',
+                                     'gyro_x', 'gyro_y', 'gyro_z']].values
+        segments.append(window)
+        labels.append(label)
+    return segments, labels
 
+X_train_segments = []
+y_train_segments = []
+X_test_segments = []
+y_test_segments = []
 
-for filename in training_files:
-    create_df(filename, X_train)
+for filename, label in zip(training_files, labels):
+    df = create_df(filename, [])
+    # segments, labels = segmentation(df, label=label)
+    # print(segments)
+    # print(labels)
+    # break
 
 for filename in test_files:
-    create_df(filename, X_test)
-
+    df = create_df(filename, [])
---
+++ data_process/scratch.py
@@ -0,0 +1,8 @@
+
+
+def helper(bla, listy):
+    listy.append("1")
+    return listy
+
+mylist = helper(2, [])
+print(mylist)
